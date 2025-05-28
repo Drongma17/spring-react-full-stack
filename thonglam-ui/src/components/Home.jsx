6dda4ed7a1370ca12f61b8ba5bf758a6aad12b15
ngawang2yeshi@gmail.com <awan32600>
2025-05-28 08:42:55 -0500

    sorting searching

--- thonglam-ui/src/components/Home.jsx
+++ thonglam-ui/src/components/Home.jsx
@@ -1,20 +1,25 @@
-import products from "../data/products";
+
 import PageHeading from "./PageHeading";
 import ProductListing from "./ProductListing";
-
+import apiClient from "../api/apiClient";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const isActive = Math.random() >0.5;
 
-  const h1Styling ={
-  textAlign : "center",
-  color: isActive ? "black": "red"
-}
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    fetchProduct()
+  }, [])
+ 
+  const fetchProduct = async() =>{
+    const response = await apiClient.get("/products"); // axios GET Reqeust
+    setProducts(response.data);
+  }
   return (
    <>
    <h1 className={`my-heading ${isActive? "primary-color": "secondary-color"}`}>Demo of Global CSS Scope from Home</h1>
-   {/* <h1 className="my-heading">Demo of Global CSS Scope from Home</h1> */}
-   {/* <h1 style={h1Styling}>Demo of Global CSS Scope from Home</h1> */}
+
     <div className="home-container">
       <PageHeading title="Explore Eazy Sticker">welcome to this new project!</PageHeading>
       <ProductListing  products={products}/>
